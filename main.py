93f16fa98932c54e3b2e9e144be1eea97cab6bd7
Marcus <marcusvgcardona@gmail.com>
2023-05-20 20:06:26 -0300

    primeira versão completa

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import random
 from pathlib import Path
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, session
 
 songs = [file.stem for file in Path("./static/assets").iterdir()]
 app = Flask(__name__)
+app.secret_key = 'thisIsSecret'
 
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
-    chosen_song = random.choice(songs)
-    if request.method == "POST":
-        guess = request.form["guess"]
-        return redirect(url_for("result", guess=guess, chosen_song=chosen_song))
-    else:
-        song_url = url_for("static",filename=f"assets/{chosen_song}.webm")
-        return render_template("index.html", song_url=song_url)
-
-@app.route("/result/<chosen_song>/<guess>")
-def result(guess, chosen_song):
+    # if request.method == "GET":
+    session['chosen_song'] = random.choice(songs)
+    song_url = url_for("static",filename=f"assets/{session['chosen_song']}.webm")
+    return render_template("index.html", song_url=song_url)
+    
+@app.route("/result/", methods=["POST"])
+def result():
+    guess = request.form["guess"]
+    chosen_song = session["chosen_song"]
     if guess == chosen_song:
         result_message = "You got it!"
     else:
